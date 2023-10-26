53a87ddc1129249f411f18cac66d84b76e6b9ba4
MichaelTimbert <110017095+MichaelTimbert@users.noreply.github.com>
2023-10-27 00:27:00 +0200

    simple database in json file

---
+++ src/database.rs
@@ -0,0 +1,55 @@
+use crate::Transaction;
+use std::fs::OpenOptions;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct DataBase {
+    transactions: Vec<Transaction>,
+}
+
+
+
+impl DataBase{
+    pub fn load(file: &std::path::PathBuf) -> DataBase{
+        //info!("Loading database '{}'", file.display());
+        if file.exists() {
+            // info!("loading database from {}",file);
+            //debug!("Loading from existing file");
+            let dbf = OpenOptions::new().read(true).open(file).unwrap();
+
+            let db: DataBase = serde_json::from_reader(dbf).unwrap();
+
+            db
+        } else {
+            //debug!("Creating new database");
+            Default::default()
+        }
+    }
+    
+    pub fn store(self, path: &std::path::PathBuf) {
+        let dbf = OpenOptions::new()
+        .create(true) // create it if doesnt existe
+        .truncate(true) // if it alredy existe overwrite it
+        .write(true)
+        .open(path)
+        .unwrap(); // TODO : catch
+
+        // TODO : Catch Result
+        let _ = serde_json::to_writer(dbf, &self);
+    }
+
+    pub fn last_transaction_id(&self) -> usize{
+        let mut last_id = 0;
+        self.transactions.iter().for_each(|tr| if tr.id > last_id { last_id=tr.id});
+        last_id
+    }
+
+    pub fn new_id(&self) -> usize {
+        self.last_transaction_id() + 1
+    }
+
+    pub fn add_transaction(&mut self, mut tr: Transaction) {
+        tr.id = self.new_id();
+        self.transactions.push(tr);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use clap::{Parser, Subcommand};
 use chrono::NaiveDate;
 use rust_decimal::Decimal;
-
+mod transaction;
+use transaction::Transaction;
+mod database;
+use database::DataBase;
 
 #[derive(Parser, Debug)]
 struct Cli {
     // command
     #[command(subcommand)]
     cmd: Commands,
+    /// Sets a default database file
+    #[arg(
+        long,
+        value_name = "database file",
+        default_value = "money.db",
+        global = true
+    )] // allow to use --db after subcommand
+    dbfile: std::path::PathBuf,
 }
 
 #[derive(Subcommand, Debug)]
@@ -18,10 +29,20 @@ enum Commands {
     },
 }
 
+
+
 fn main() {
     let args = Cli::parse();
 
+    let mut db = DataBase::load(&args.dbfile);
+
     match args.cmd {
-        Commands::Add { date, amount } => { println!("adding transaction of {amount} on {date}") }
+        Commands::Add { date, amount } => {
+            let new_transaction = Transaction{id:0, date, amount};
+            println!("adding transaction {new_transaction:?}");
+            db.add_transaction(new_transaction);
+        }
     }
+
+    db.store(&args.dbfile);
 }
